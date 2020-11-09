89df97f540217f2ddb10d761ac555971af9a5819
verissimo-sn <verissimo_sn@hotmail.com>
2020-11-09 11:44:30 -0300

    feat(encrypt/password): encrypt user password

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb+srv://jwt-auth-login:devlogin@cluster0.4gtzn.mongodb.net/test-jwt-auth');
+mongoose
+.connect('mongodb+srv://jwt-auth-login:devlogin@cluster0.4gtzn.mongodb.net/test-jwt-auth',
+{
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 mongoose.Promise = global.Promise;
 
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,4 +1,14 @@
+import bcrypt from 'bcrypt';
+import { Document } from 'mongoose';
+
 import mongoose from '../database/index';
+
+interface User extends Document {
+  name: string;
+  email: string;
+  password: string;
+  createdAt: string;
+}
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -22,6 +32,15 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.pre<User>('save', async function(next) {
+  const hashSalt = 10;
+  const hash = await bcrypt.hash(this.password, hashSalt);
+
+  console.log(hash);
+  this.password = hash;
+  next();
+});
+
+const User = mongoose.model<User>('User', UserSchema);
 
 export default User;
