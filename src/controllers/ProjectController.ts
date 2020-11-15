c58e224f49fa13b97f1c48ed3971d511de281605
verissimo-sn <verissimo_sn@hotmail.com>
2020-11-15 17:39:28 -0300

    ajustes

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -2,7 +2,10 @@ import { Request, Response } from 'express';
 
 class ProjectController {
   index(req: Request, res: Response) {
-    return res.send({ message: 'Authenticated' });
+    // const user = userId;
+
+    res.send({ message: 'Authenticated' });
+
   }
 }
 
--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -13,20 +13,22 @@ interface Decoded {
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
-  if(!authHeader) return res.status(401).send({ error: 'No token provided' });
+  if(!authHeader) return res.status(401).send({ error: 'Token not provided' });
 
   const [, token] = authHeader.split(' ');
 
   jwt.verify(token, SECRET, (err, decoded) => {
     if(err) return res.status(400).send({ error: 'Invalid token' });
 
-    res.send({
-      userId: (decoded as Decoded).id
-    });
+    const userId = (decoded as Decoded).id;
+
+    // (req as UserReq).userId = (decoded as Decoded).id;
+
+    // res.send({ userId });
+
+
+    return next();
   });
-  
-  //falta enviar o id do usuário para a rota autenticada 
-  return next();
 }
 
 export default authMiddleware;
